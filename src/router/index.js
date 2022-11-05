884d8a2818fc238683ddf2600818c79a73d8c545
Elisha Benjamin <malishben360@gmail.com>
2022-11-05 08:43:39 +0100

    Simple vue form

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import SimpleForm from "../views/SimpleForm.vue";
+import ComponentForm from "../views/ComponentForm.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "SimpleForm",
+    component: SimpleForm,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/component-form",
+    name: "ComponentForm",
+    component: ComponentForm,
   },
 ];
 
